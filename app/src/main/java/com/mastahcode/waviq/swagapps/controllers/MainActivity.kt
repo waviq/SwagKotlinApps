230e657c87be41d59ff8a4a29f637c5f77f0b7ca
Waviq Subhi <waviq.subkhi@gmail.com>
2017-11-08 23:05:25 +0700

    Simple List View on Lesson-02

--- app/src/main/java/com/mastahcode/waviq/swagapps/controllers/MainActivity.kt
+++ app/src/main/java/com/mastahcode/waviq/swagapps/controllers/MainActivity.kt
@@ -2,12 +2,21 @@ package com.mastahcode.waviq.swagapps.controllers
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.mastahcode.waviq.swagapps.R
+import com.mastahcode.waviq.swagapps.models.Category
+import com.mastahcode.waviq.swagapps.services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var adapter : ArrayAdapter<Category>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.id.categoryImage, DataService.categories)
+
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/com/mastahcode/waviq/swagapps/models/Category.kt
+++ app/src/main/java/com/mastahcode/waviq/swagapps/models/Category.kt
@@ -4,4 +4,8 @@ package com.mastahcode.waviq.swagapps.models
  * Created by Waviq on 11/8/17.
  *
  */
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
